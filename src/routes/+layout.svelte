bf0905c7313576eaaf0b82f47c557c0fd276c15d
Carl Ragunton <70619975+OozoraCielo@users.noreply.github.com>
2023-05-16 21:02:11 +0800

    Add files via upload

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+    import '../app.css';
+    import Navbar from "../components/navbar.svelte";
+    import Pagebase from '../components/pagebase.svelte';
+
+
+</script>
+
+<Navbar>
+</Navbar>
+<Pagebase>
+</Pagebase>
+
+<slot></slot>
